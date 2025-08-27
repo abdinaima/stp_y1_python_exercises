ddfaf322acc8bb7a9ce6256c0f56dc23f7b2b6b8
abdinaima <abdi.naima@outlook.com>
2025-08-27 18:38:16 +0100

    Initial attempt at creating a resusable function for this exercise

---
+++ exercise_1.4_blocking.py
@@ -0,0 +1,20 @@
+
+"""This function is called 'blocking'.
+It takes 2 arguments, a DNA sequence 'seq' and an integer 'blocksize'.
+The function then prints the sequence in blocks of the desired size, with gaps seperating the blocks."""
+
+
+def blocking (seq, blocksize):
+
+    i = 0
+    blocks = []
+
+    while i < len(seq):
+        blocks.append(seq[i:i+blocksize])
+        i +=blocksize
+    
+    print(" ".join(blocks))
+
+if __name__ == "__main__":
+    blocking("aggagtaagcccttgcaactggaaatacacccattg", 3)
+
