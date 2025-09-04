e0a3ab6c8bc9a3e69384e3dbe9cd2b4083603a72
abdinaima <abdi.naima@outlook.com>
2025-09-04 11:53:08 +0100

    Adjusted return statement to be truly inclusive of 2nd integer in the pair, and added ValueError for non-DNA bases

--- exercise_1/two_slices.py
+++ exercise_1/two_slices.py
@@ -7,27 +7,30 @@ The function returns 2 slices of the DNA sequence, between the positions represe
 
 
 def two_slices (seq, n1, n2, n3, n4):
+
+    seq = seq.upper()
+
+    allowed_bases = {"A", "T", "C", "G"}
     
     if not isinstance (seq, str):
         raise TypeError ("Sequence must be a string!")
-
+    
+    for x in seq:
+        if x not in allowed_bases:
+            raise ValueError ("Invalid non-DNA bases appear in this sequence!")
+        
     if not isinstance(n1,int) or not isinstance(n2,int) or not isinstance(n3,int) or not isinstance(n4,int):
         raise TypeError ("All 4 numbers must be integers!")
     
     if n1 <0 or n2<0 or n3<0 or n4<0:
         raise ValueError ("All 4 numbers must be positive!")
 
-    return seq[n1:n2] + " " + seq[n3:n4]
+    return seq[n1:n2+1] + " " + seq[n3:n4+1]
     
 
 if __name__ == "__main__":
     
-    seq = """GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAG
-GAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCT
-TCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTC
-TGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTG
-AAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAG
-GAGCCTACAAGAAAGTACGAGATTTGAT"""
+    seq = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTGAT"
     
     n1 = 137
     n2 = 143
